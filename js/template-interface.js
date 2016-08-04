af7874ebdd4bee76ab650e71e3ba0f17538df316
Brett New and Stewart Cole <brett+stewart@>
2016-08-04 11:56:02 -0700

    more messing around to get random tracks to list and embed.

--- js/template-interface.js
+++ js/template-interface.js
@@ -1,19 +1,23 @@
-
+var currentList = [];
 
 $(document).ready(function() {
   SC.initialize({
     client_id: '7cf07043235f0e04f0a9bc58300b6c07',
     redirect_uri: 'login-dummie.html'
   });
-  $('#userConnect').click(function(event){
-    event.preventDefault();
-    debugger;
+  $('#userConnect').click(function () {
     SC.connect(function () {
-      SC.get("/me", function (response) {
-        console.log("Welcome" + response.username);
-      });
+        SC.get("/me", function (me) {
+            user_perma = me.permalink;
+            setUI(me.username, me.avatar_url, me.description);
+        });
+        if (SC.isConnected) {
+            console.log("logged in");
+        }
+        getTracks();
+        getPlaylists();
     });
-  });
+});
 
   $("#embedTrack").click(function(event) {
      event.preventDefault();
@@ -25,27 +29,39 @@ $(document).ready(function() {
       maxheight: 200
     }, function(res) {
       $("#player").html(res.html);
+
     });
   });
+
     $("#getList").click(function(event){
       event.preventDefault();
-      var artist = $("#querySong").val();
-      artist = artist.split(" ").join("");
-      SC.get("/" + artist, function(response){
+      SC.get("/tracks", function(response){
+        for (var i = response.length-1; i >= 0; i --){
+          currentList.push(response[i]);
+          SC.oEmbed(response[i].permalink_url, {
+            maxheight: 400
+          }, function(res){
+            $('#player').html(res.html);
+          });
+        }
         for (var i = 0; i < response.length; i++) {
-          $("ul").append("<li>" + response[i].title + "</li>");
+          $("#random").append("<li>" + currentList[i].title + "</li>");
         }
       });
     });
-  // $()
-  //   SC.stream("/tracks/293", function(sound) {
-  //       $('#start').click(function(e) {
-  //           e.preventDefault();
-  //           sound.start;
-  //       });
-  //       $('#stop').click(function(e) {
-  //           e.preventDefault();
-  //           sound.stop();
-  //       });
-  //     });
+
+    $('#skip').click(function(event) {
+      event.preventDefault();
+
+    })
+    SC.stream("/tracks", function(sound) {
+        $('#start').click(function(e) {
+            e.preventDefault();
+            sound.start();
+        });
+        $('#stop').click(function(e) {
+            e.preventDefault();
+            sound.stop();
+        });
+      });
 });
